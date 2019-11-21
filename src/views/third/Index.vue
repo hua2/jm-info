a3251961554b2f429250defa860b5ab0a6014bf0
yuan1 <limingyuan1996@gmail.com>
2019-11-21 17:54:35 +0800

    add countdown

--- src/views/third/Index.vue
+++ src/views/third/Index.vue
@@ -33,7 +33,23 @@
         </div>
       </div>
       <div class="t-c-right flex flex-col">
-        <div id="app">{{ `${sec}分钟` }}</div>
+        <div class="third-countdown">
+          <div class="t-c-cn">倒计时</div>
+          <div class="t-c-en">COUNT DOWN</div>
+          <i class="t-c-io"></i>
+          <div class="t-c-info">距离结束还剩</div>
+          <div class="t-c-count">
+            <div class="t-c-c-hour">
+              <span class="t-c-c-txt">{{ now | moment("HH") }}</span>
+            </div>
+            <div class="t-c-c-min">
+              <span class="t-c-c-txt">{{ now | moment("mm") }}</span>
+            </div>
+            <div class="t-c-c-sec">
+              <span class="t-c-c-txt">{{ now | moment("ss") }}</span>
+            </div>
+          </div>
+        </div>
         <div>
           <div>类别 <span>公制(Metric)</span></div>
           <div>系列 <span>粗牙</span></div>
@@ -58,32 +74,35 @@ export default {
   name: "Index",
   data() {
     return {
-      day: 0,
-      hr: 0,
-      min: 0,
-      sec: 0
+      now: new Date(),
+      countdownIntervalId: null,
+      countdownTimeoutId: null
     };
   },
-  mounted: function() {
+  created() {
+    this.now.setHours(0, 10, 0, 0);
     this.countdown();
+    this.autoStopCountdown();
   },
   methods: {
-    countdown: function() {
-      const end = Date.parse(new Date("2019-11-21"));
-      const now = Date.parse(new Date());
-      const msec = end - now;
-      // let day = parseInt(msec / 1000 / 60 / 60 / 24);
-      // let hr = parseInt((msec / 1000 / 60 / 60) % 24);
-      let min = parseInt((msec / 1000 / 60) % 60);
-      let sec = parseInt((msec / 1000) % 60);
-      // this.day = day;
-      // this.hr = hr > 9 ? hr : "0" + hr;
-      this.min = min > 9 ? min : "0" + min;
-      this.sec = sec > 9 ? sec : "0" + sec;
-      const that = this;
-      setTimeout(function() {
-        that.countdown();
+    countdown() {
+      this.countdownIntervalId = setInterval(() => {
+        this.now.setSeconds(this.now.getSeconds() - 1);
+        this.$forceUpdate();
       }, 1000);
+    },
+    autoStopCountdown() {
+      this.countdownTimeoutId = setTimeout(() => {
+        clearInterval(this.countdownIntervalId);
+      }, 1000 * 60 * 10);
+    }
+  },
+  beforeDestroy() {
+    if (this.countdownIntervalId) {
+      clearInterval(this.countdownIntervalId);
+    }
+    if (this.countdownTimeoutId) {
+      clearTimeout(this.countdownTimeoutId);
     }
   }
 };
@@ -94,6 +113,7 @@ export default {
   width: 100%;
   margin-top: 48px;
   padding: 0 100px;
+
   p {
     font-weight: 500;
     font-size: 24px;
@@ -118,6 +138,91 @@ export default {
     .t-c-right {
       margin-left: 96px;
       /*background: #eeeeee;*/
+    }
+  }
+
+  .third-countdown {
+    width: 190px;
+    height: 275px;
+    background-color: #e83632;
+    position: relative;
+    .t-c-cn {
+      position: absolute;
+      top: 42px;
+      left: 0;
+      width: 100%;
+      text-align: center;
+      font-size: 34px;
+      color: #fff;
+    }
+
+    .t-c-en {
+      position: absolute;
+      top: 90px;
+      left: 0;
+      width: 100%;
+      text-align: center;
+      font-size: 20px;
+      color: rgba(255, 255, 255, 0.5);
+    }
+
+    .t-c-io {
+      width: 20px;
+      height: 33px;
+      position: absolute;
+      background: url("../../assets/img/seckill.png") no-repeat -32.5px 0;
+      background-size: 52.5px 40px;
+      left: 85px;
+      top: 126px;
+      display: block;
+    }
+
+    .t-c-info {
+      position: absolute;
+      top: 170px;
+      text-align: center;
+      width: 100%;
+      font-size: 16px;
+      color: #fff;
+    }
+
+    .t-c-count {
+      position: absolute;
+      top: 212px;
+      left: 30px;
+      height: 40px;
+      .t-c-c-hour,
+      .t-c-c-min,
+      .t-c-c-sec {
+        position: relative;
+        background-color: #2f3430;
+        width: 40px;
+        height: 40px;
+        float: left;
+        text-align: center;
+        line-height: 40px;
+        margin-right: 5px;
+      }
+
+      .t-c-c-txt {
+        font-size: 20px;
+        font-weight: bold;
+        color: #fff;
+        height: auto;
+        width: auto;
+        margin: 0;
+        background: unset;
+        &:before {
+          content: "";
+          display: block;
+          position: absolute;
+          top: 50%;
+          left: 0;
+          width: 100%;
+          height: 1px;
+          background-color: #e83632;
+        }
+      }
     }
   }
 }
